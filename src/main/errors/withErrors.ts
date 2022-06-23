15b61bbf67be5336e174ac267ed6f60d978446ec
smikhalevski <smikhalevski@gmail.com>
2022-06-24 02:16:00 +0300

    Fixed withErrors subscription

--- src/main/errors/withErrors.ts
+++ src/main/errors/withErrors.ts
@@ -3,16 +3,42 @@ import {Enhancer} from '../Field';
 
 export interface WithErrors<T = any> {
 
-  error: T | undefined;
-  invalid: boolean;
+  /**
+   * An error associated with this field, or `undefined` if there's no error.
+   */
+  readonly error: T | undefined;
 
+  /**
+   * `true` if the field has an error.
+   */
+  readonly invalid: boolean;
+
+  /**
+   * Associates the error with this field.
+   *
+   * @param error The error to set.
+   */
   setError(error: T): void;
 
+  /**
+   * Clear the error associated with this field.
+   */
   clearError(): void;
 }
 
+/**
+ * Enhances the field with the error-related properties.
+ *
+ * @param errors The map that associates field and a validation error.
+ */
 export function withErrors<T>(errors: Errors<T>): Enhancer<WithErrors<T>> {
   return (field: any) => {
+
+    errors.subscribe((targetField) => {
+      if (targetField === field) {
+        field.notify();
+      }
+    });
 
     field.setError = (error: T) => {
       errors.set(field, error);
