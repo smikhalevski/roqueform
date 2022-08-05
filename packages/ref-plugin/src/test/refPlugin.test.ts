4237dba530fffd8f6df039d6457c1454551e9b9e
smikhalevski <smikhalevski@gmail.com>
2022-08-05 20:46:12 +0300

    Minor simplification

--- packages/ref-plugin/src/test/refPlugin.test.ts
+++ packages/ref-plugin/src/test/refPlugin.test.ts
@@ -1,11 +1,11 @@
-import { renderHook } from '@testing-library/react';
-import { useField } from 'roqueform';
+import { createField, objectAccessor } from 'roqueform';
 import { refPlugin } from '../main';
 
 describe('refPlugin', () => {
   test('adds ref property to the field', () => {
-    const hook = renderHook(() => useField(111, refPlugin()));
+    const field = createField(objectAccessor, { bar: 111 }, refPlugin());
 
-    expect(hook.result.current.ref).toEqual({ current: null });
+    expect(field.ref).toEqual({ current: null });
+    expect(field.at('bar').ref).toEqual({ current: null });
   });
 });
