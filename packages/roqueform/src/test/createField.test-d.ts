76947ec6b9c29da94764e10e730bd93deb7f05c4
smikhalevski <smikhalevski@gmail.com>
2023-02-21 17:40:18 +0300

    Consolidate discriminated unions

--- packages/roqueform/src/test/createField.test-d.ts
+++ packages/roqueform/src/test/createField.test-d.ts
@@ -1,8 +1,20 @@
 import { expectType } from 'tsd';
 import { createField, objectAccessor } from 'roqueform';
 
-const field = createField<{ foo: { bar?: string } | null }>(objectAccessor, { foo: null });
+// Optional properties
 
-expectType<{ bar?: string } | null>(field.at('foo').value);
+const field1 = createField<{ foo: { bar?: string } | null }>(objectAccessor, { foo: null });
 
-expectType<string | undefined>(field.at('foo').at('bar').value);
+expectType<{ bar?: string } | null>(field1.at('foo').value);
+
+expectType<string | undefined>(field1.at('foo').at('bar').value);
+
+// Unions
+
+type Field2Value = { aaa: number } | { aaa: boolean; bbb: string };
+
+const field2 = createField<Field2Value>(objectAccessor, { aaa: 111 });
+
+expectType<number | boolean>(field2.at('aaa').value);
+
+expectType<string | undefined>(field2.at('bbb').value);
