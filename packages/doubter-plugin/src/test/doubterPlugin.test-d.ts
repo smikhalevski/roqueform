3bf02193072c41e17764db5831f711ae1f0a7c9d
smikhalevski <smikhalevski@gmail.com>
2024-02-06 17:35:02 +0300

    Fixed publish

--- packages/doubter-plugin/src/test/doubterPlugin.test-d.ts
+++ packages/doubter-plugin/src/test/doubterPlugin.test-d.ts
@@ -1,13 +1,8 @@
-import { annotationsPlugin, AnnotationsPlugin } from '@roqueform/annotations-plugin';
 import { DoubterPlugin, doubterPlugin } from '@roqueform/doubter-plugin';
 import * as d from 'doubter';
-import { composePlugins, createField, Field } from 'roqueform';
+import { createField, Field } from 'roqueform';
 import { expectType } from 'tsd';
 
 const shape = d.object({ aaa: d.object({ bbb: d.string() }) });
 
 expectType<Field<{ aaa: { bbb: string } }, DoubterPlugin>>(createField({ aaa: { bbb: 'aaa' } }, doubterPlugin(shape)));
-
-expectType<Field<{ aaa: { bbb: string } }, AnnotationsPlugin<{ xxx: string }> & DoubterPlugin>>(
-  createField({ aaa: { bbb: 'aaa' } }, composePlugins(doubterPlugin(shape), annotationsPlugin({ xxx: 'yyy' })))
-);
